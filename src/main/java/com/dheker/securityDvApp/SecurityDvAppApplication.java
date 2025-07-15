1511132b3a0e93df63af67c12938efbd10d54baf
Dheker-Laadhibi <dhekerlaadhibii@gmail.com>
2025-07-15 04:23:13 +0100

    initial commit

---
+++ src/main/java/com/dheker/securityDvApp/SecurityDvAppApplication.java
@@ -0,0 +1,13 @@
+package com.dheker.securityDvApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dheker/securityDvApp/SecurityDvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheker.securityDvApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
